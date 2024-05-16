2b6213c2c409a802c5fec73add6cdfba0e0ffe1c
JoseMgt23 <joseclasef3ae@gmail.com>
2024-05-15 21:59:10 -0500

    Implementando El API

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/propiedades', [PropiedadController::class, 'index'])->name('propiedades');
