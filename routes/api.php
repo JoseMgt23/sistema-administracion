a855538114cd3a61107830b2c52745b449d3eb15
JoseMgt23 <joseclasef3ae@gmail.com>
2024-05-16 18:15:40 -0500

    [Backend]  Rutas API PropiedadController

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) 
+{
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 
+Route::post('/propiedades', [PropiedadController::class, 'store'])->name('propiedades.store');
 Route::get('/propiedades', [PropiedadController::class, 'index'])->name('propiedades');
+Route::delete('/propiedades/{propiedad}' , [PropiedadController::class, 'destroy'])->name('propiedades.destroy');
+Route::get('/propiedades/{propiedad}' , [PropiedadController::class, 'show'])->name('propiedades.show');
+Route::put('/propiedades/{propiedad}' , [PropiedadController::class, 'update'])->name('propiedades.update');
