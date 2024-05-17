c9906e2fd0755a8151cd9a9090eac5536db4bdf1
JoseMgt23 <joseclasef3ae@gmail.com>
2024-05-16 21:24:16 -0500

    [Backend]  Rutas API ArrendatarioController

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,10 @@ Route::get('/propiedades', [PropiedadController::class, 'index'])->name('propied
 Route::delete('/propiedades/{propiedad}' , [PropiedadController::class, 'destroy'])->name('propiedades.destroy');
 Route::get('/propiedades/{propiedad}' , [PropiedadController::class, 'show'])->name('propiedades.show');
 Route::put('/propiedades/{propiedad}' , [PropiedadController::class, 'update'])->name('propiedades.update');
+
+
+Route::post('/arrendatarios', [ArrendatarioController::class, 'store'])->name('arrendatarios.store');
+Route::get('/arrendatarios', [ArrendatarioController::class, 'index'])->name('arrendatarios.index');
+Route::delete('/arrendatarios/{arrendatario}', [ArrendatarioController::class, 'destroy'])->name('arrendatarios.destroy');
+Route::get('/arrendatarios/{arrendatario}', [ArrendatarioController::class, 'show'])->name('arrendatarios.show');
+Route::put('/arrendatarios/{arrendatario}', [ArrendatarioController::class, 'update'])->name('arrendatarios.update');
